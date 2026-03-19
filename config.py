6a791bcb33418870b4d24289086bc5b50757e461
avruchsource <yuvalavr@gmail.com>
2026-03-19 19:34:32 +0200

    19Mar_v1

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent / ".env",
+        env_file_encoding="utf-8",
+    )
 
     BOT_TOKEN: str
     ADMIN_CHAT_ID: int
